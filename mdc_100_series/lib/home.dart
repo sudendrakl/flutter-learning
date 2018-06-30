bbce46b6cd6017687cba727f5edef69444807ce9
Sudendra Kamble <sudendra@localhost.localdomain>
2018-07-01 00:25:23 +0530

    list adding

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,14 +13,125 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/data.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
+  ScrollController _scrollController = ScrollController();
+  int _scrollDuration = 1000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search Button');
+              _scrollController.animateTo(
+                  _scrollController.position.maxScrollExtent,
+                  duration: Duration(seconds: _scrollDuration),
+                  curve: Curves.linear);
+              print ('going bottom');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alert),
+            onPressed: () {
+              print('Alarm Button');
+              _scrollController.animateTo(
+                  _scrollController.position.minScrollExtent,
+                  duration: Duration(seconds: _scrollDuration),
+                  curve: Curves.linear);
+              print('going top');
+            },
+          )
+        ],
+        leading: Icon(Icons.menu),
+        title: Text('SHRINE'),
+        centerTitle: true,
+        elevation: 6.0,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _multiply(context), //_buildGridCards(context),
+        controller: _scrollController,
       ),
     );
+  }
+
+  List<Card> _multiply(BuildContext context) {
+    List<Card> multipliedList = _buildGridCards(context, 0);
+    for (var i = 1; i < 500; i++) {
+      multipliedList.addAll(_buildGridCards(context, i));
+    }
+    return multipliedList;
+  }
+
+  List<Card> _buildGridCards(BuildContext context, int page) {
+    List<Product> products = getProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData themeData = Theme.of(context);
+    final NumberFormat format = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    List<Card> cards = products.map((product) {
+      return Card(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                  aspectRatio: 18.0 / 9.0,
+                  child: Stack(
+                    children: <Widget>[
+                      Image.asset(
+                        product.assetName,
+                        package: product.assetPackage,
+                        fit: BoxFit.fitWidth,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(3.0),
+                        child: Text(
+                          "page $page",
+                          style: themeData.textTheme.caption,
+                          maxLines: 1,
+                          textAlign: TextAlign.right,
+                        ),
+                      )
+                    ],
+                    fit: StackFit.passthrough,
+                  )),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: themeData.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      format.format(product.price),
+                      style: themeData.textTheme.body2,
+                    )
+                  ],
+                ),
+              )
+            ]),
+      );
+    }).toList();
+
+    return cards;
   }
 }
--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -20,24 +20,60 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void clear() {
+    _userNameController.clear();
+    _passwordController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new SafeArea(
         child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            const SizedBox(height: 80.0),
-            new Column(
-              children: <Widget>[
-                new Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
-              ],
-            ),
-            const SizedBox(height: 120.0),
-          ],
-        ),
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              const SizedBox(height: 80.0),
+              new Column(
+                children: <Widget>[
+                  new Image.asset('assets/diamond.png'),
+                  const SizedBox(height: 16.0),
+                  const Text('SHRINE'),
+                ],
+              ),
+              const SizedBox(height: 100.0),
+              TextField(
+                decoration:
+                    InputDecoration(filled: true, labelText: 'Username'),
+                keyboardType: TextInputType.emailAddress,
+                key: Key('uname'),
+                controller: _userNameController,
+              ),
+              SizedBox(height: 10.0),
+              TextField(
+                decoration:
+                    InputDecoration(filled: true, labelText: 'Password'),
+                obscureText: true,
+                key: Key('passwd'),
+                controller: _passwordController,
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('CANCEL'),
+                    onPressed: clear,
+                  ),
+                  RaisedButton(
+                      child: Text('NEXT'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      })
+                ],
+              )
+            ]),
       ),
     );
   }
